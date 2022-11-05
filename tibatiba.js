8d4ce74697cc93cd4ecee3b382bfc524a8b553e1
Aftermath00 <rizkyswandy@gmail.com>
2022-11-06 01:00:51 +0700

    Modifikasi about me

--- tibatiba.js
+++ tibatiba.js
@@ -35,7 +35,7 @@ for (let i = 0; i < tabPanes.length; i++) {
     _class("tab-header")[0].getElementsByClassName("active")[0].classList.remove("active");
     tabPanes[i].classList.add("active");
 
-    _class("tab-line")[0].style.top = calc("80px + ${i*50}px");
+    _class("tab-line")[0].style.top = "calc(80px + ${i*50}px)";
 
     _class("tab-content")[0].getElementsByClassName("active")[0].classList.remove("active");
     tabPanes[i].classList.add("active");
